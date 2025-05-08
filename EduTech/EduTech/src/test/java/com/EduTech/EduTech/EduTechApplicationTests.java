103c27ae2e9d1ef6207d95918eb42e4c80162fef
Nicolas Maureira <ni.maureira@duocuc.cl>
2025-05-08 13:11:04 -0400

    Add files via upload

---
+++ EduTech/EduTech/src/test/java/com/EduTech/EduTech/EduTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EduTech.EduTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
